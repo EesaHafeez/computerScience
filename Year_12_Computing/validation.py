5683c54895291c352f9d2e2b21de79e4801d78d4
EesaHafeez <19hafeeze@centralfoundationboys.co.uk>
2024-09-25 16:58:28 +0000

    subroutine added

--- Year_12_Computing/validation.py
+++ Year_12_Computing/validation.py
@@ -2,12 +2,25 @@
 database= [['eesahafeez', '1234', 'qwerty'], 
            ['qwertyuiop', '4321','asdfg' ]]
 
+def database_search():
+    for i in range(len(database)):
+        if database[i][0] == id:
+            return True
+        else:
+            return False
+  
+
+
 correct_id = False
 id= input('Enter a character id: ')
+correct_id = database_search()
+print(correct_id)
 id_check=0
-while id_check != 2 and len(id) != 10:
-    id_check = id_check +1
-    id= input('Enter a character id of 10 charcters: ')
+while id_check != 2 and len(id) != 10 :
+    if correct_id == False:
+        correct_id = database_search()
+        id_check = id_check +1
+        id= input('Enter a character id of 10 charcters: ')
 if id_check<2:
     for i in range(len(database)):
         if database[i][0] == id:
@@ -26,6 +39,13 @@ if correct_id == True:
         corect_pin = False
         if database[list][1] == pin:
             correct_pin == True
+
+def databse_search():
+    for i in range(len(database)):
+        if database[i][0] == id:
+            return True
+  
+
 
 
 
