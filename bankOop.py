87498432a9410e2389e5e80efb254b41f09db379
EesaHafeez <19hafeeze@centralfoundationboys.co.uk>
2024-11-26 10:27:02 +0000

    changes

---
+++ bankOop.py
@@ -0,0 +1,10 @@
+'''Create a Python class for a Bank. Implement a constructor that initializes an empty list to store bank accounts.
+ Create methods to add a new account to the bank, find an account by account number, deposit money into an account,
+   withdraw money from an account, and display all accounts' information. Instantiate more than one object from the class, 
+   and show suitable testing. '''
+
+class Bank:
+    
+
+
+    def __init__()
--- studentOop.py
+++ studentOop.py
@@ -11,4 +11,37 @@ Instantiate more than one object from the class, and show suitable testing.  '''
 
 class Student:
     # set private attributes
-    hello = None
+    __studentName = None
+    __id = None
+    __universityMajor = None
+    # constructor method
+    def __init__(self, theStudentName, theId, theUniversityMajor):
+        self.__studentName = theStudentName
+        self.__id = theId
+        self.__universityMajor = theUniversityMajor
+    # method to get university major
+    def getUniversityMajor(self):
+        return self.__universityMajor
+    # method to set university major    
+    def setUniversityMajor(self,theUniversityMajor):
+        self.__universityMajor = theUniversityMajor
+    # method to display all information  
+    def displayInformation(self):
+        print(self.__studentName, self.__id, self.__universityMajor)
+
+#################################### --> Main Program <-- ######################################
+# instantiating objects student1 and student2 using the class Student
+student1 = Student('Eesa', 1416, 'Computing')
+student2 = Student('Husaam', 1234, 'English')
+
+# using mehtod to dispplay inforamtion of both objects
+student1.displayInformation()
+student2.displayInformation()
+
+# using method to set and get university major 
+student1.setUniversityMajor('Maths')
+student1.displayInformation()
+if student1.getUniversityMajor() == 'Maths':
+    print('You are not doing a computing degree')
+
+
