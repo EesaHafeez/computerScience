d5c37986468fb6b365335c8d44a18718eb88d3e1
EesaHafeez <19hafeeze@centralfoundationboys.co.uk>
2024-11-28 14:10:56 +0000

    pygame

---
+++ pygame.py
@@ -0,0 +1,42 @@
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+while running:
+    # poll for events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill((13,56,89))
+
+    # Draw the circle, converting player_pos to integers
+    pygame.draw.circle(screen, ((12,34,56)), (int(player_pos.x), int(player_pos.y)), 40)
+
+    # Handle player movement
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+
+    # Update the display
+    pygame.display.flip()
+
+    # Limit FPS to 60
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
+
