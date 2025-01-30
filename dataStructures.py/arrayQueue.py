019407026ee82676289ceff99f04d56c0eaf6512
EesaHafeez <19hafeeze@centralfoundationboys.co.uk>
2025-01-30 14:26:00 +0000

    arrayQueue

--- dataStructures.py/arrayQueue.py
+++ dataStructures.py/arrayQueue.py
@@ -3,6 +3,12 @@ front = 0
 rear = -1
 size = 0
 maxSize = 5
+def menu():
+   print('')
+   print('MENU: ')
+   print('1 - Add an item to the queue')
+   print('2 - Remove an item from the queue')
+   print('3 - Quit')
 
 def enQueue(item):
    global rear, size
@@ -10,9 +16,10 @@ def enQueue(item):
       print('Sorry queue is full')
       return
    else:
-      rear = rear + 1
+      rear = (rear + 1)%5
       busQueue[rear] = item
       size = size + 1
+   displayInfo()
 
 def deQueue():
    global front, size
@@ -22,7 +29,8 @@ def deQueue():
    else:
       cuurentFront = busQueue[front]
       size = size -1
-      front = front + 1
+      front = (front + 1)%5
+      displayInfo()
       return cuurentFront
    
 
@@ -38,20 +46,26 @@ def isEmpty():
    else:
       return False
 
-enQueue(21)
-enQueue(76)
-enQueue(141)
-enQueue(243)
-enQueue(149)
-enQueue(271)
-print(deQueue())
-print(deQueue())
-print(deQueue())
-print(deQueue())
-print(deQueue())
-print(deQueue())
+def displayInfo():
+   print(busQueue)
+   print('Front: ' + str(busQueue[front]))
+   print('Rear: ' + str(busQueue[rear]))
+   print('')
+
+
+running = True
+while running == True:
+   menu()
+   choice = int(input('Enter choice: '))
+   if choice == 1:
+      data = input('Enter item you would like to add into the queue: ' )
+      enQueue(data)
+   elif choice == 2:
+      deQueue()
+   elif choice == 3:
+      running = False
+   else:
+      print('Invalid option')
 
 
 
-
-print(busQueue)
