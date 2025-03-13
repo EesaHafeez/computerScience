bd91fdcf80bcb80ba915100d1a4a58eec220f253
EesaHafeez <19hafeeze@centralfoundationboys.co.uk>
2025-03-13 13:52:30 +0000

    stack

--- dataStructures.py/arrayLinkedList.py
+++ dataStructures.py/arrayLinkedList.py
@@ -1,24 +1,15 @@
-names = ['Husaam', 'Eesa', 'Rezwan', 'Kaya', 'Altaf']
-
-first = 4
-data = 0
-nextPointer = 1
-nextFree = 5
-
-linkedList = [
-    ['Husaam' , 3],
-    ['Eesa' , 0],
-    ['Rezwan' , None],
-    ['Kaya' , 2],
-    ['Altaf' , 1],
-    [None , None],
-]
+start = 0
 
 
-def traverse():
-    current = first
-    while current != None:
-        print(linkedList[current][data])
-        current = linkedList[current][nextPointer]
 
-traverse()
+
+
+namesLinkedList = [
+        ['Altaf'],
+        ['Mariam'],
+        ['Eesa'],
+        ['Husaam']
+        ['Sam'],
+        ['Kaya'],
+        ['Rezwan']],
+                   
